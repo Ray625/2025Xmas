bfceb5f43481c572eb58e9028c6381c626bea31c
Ray <ray.liao@clickglobal.net>
2025-11-21 11:59:19 +0800

    style: 更新popup

--- src/components/sections/Media/MediaSection.vue
+++ src/components/sections/Media/MediaSection.vue
@@ -4,7 +4,10 @@
       <div class="flex flex-col items-center">
         <SectionTitle preset="media" class="mb-[88px]" />
         <div class="media__group">
-          <div class="media__card media__card--left">
+          <div
+            class="media__card media__card--left"
+            @click.self="handleClick('https://www.youtube.com/watch?v=d51S9qXvBRI')"
+          >
             <div class="media__card__mask"></div>
           </div>
           <div class="media__card media__card--right">
@@ -18,6 +21,10 @@
 <script setup lang="ts">
 import Container from '@/components/layout/Container.vue'
 import SectionTitle from '@/components/common/SectionTitle.vue'
+
+const handleClick = (url: string) => {
+  window.open(url, '_blank', 'noopener,noreferrer')
+}
 </script>
 <style scoped lang="scss">
 @use '@/styles/_variables' as vars;
@@ -89,6 +96,12 @@ import SectionTitle from '@/components/common/SectionTitle.vue'
       background-size: contain;
       transform: translate(-50%, -50%);
     }
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .media__card--left:hover {
+    cursor: pointer;
   }
 }
 </style>
